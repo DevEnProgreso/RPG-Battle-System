4fa813ce948867f73e7255c5f1c1e17dfbb08ecd
Frede <freddyzorio12@gmail.com>
2025-05-01 19:08:55 -0600

    first and last commit

---
+++ TurnBasedBattleSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿using TurnBasedBattleSystem;
+
+Unit player = new Unit(100, 20, 12, "Player");
+Unit enemy = new Unit(75, 10, 7, "Crazy Wizard");
+Random random = new Random(); 
+
+while(!player.IsDead && !enemy.IsDead) 
+{ 
+Console.WriteLine(player.UnitName + " Hp = " + player.Hp + " " + enemy.UnitName + " HP = " + enemy.Hp);
+Console.WriteLine("Player turn! What will you do?");
+string choice = Console.ReadLine();
+
+if (choice == "a")
+{
+    player.Attack(enemy);
+    
+} else
+{
+    player.Heal();
+}
+
+    if (player.IsDead || enemy.IsDead) break;
+
+Console.WriteLine("Crazy Wizard turn!");
+
+int rand = random.Next(0, 2);
+
+if (rand == 0)
+{
+    enemy.Attack(player);   
+}
+else
+{
+     enemy.Heal();  
+}
+}
